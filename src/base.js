b204d24934960a50dee076ea0ee8721b3b0e12d5
Fil Maj <filip.maj@nitobi.com>
2011-01-07 17:50:57 -0800

    Complete dom and base docs.

--- src/base.js
+++ src/base.js
@@ -1,8 +1,8 @@
 /*
-Base
-===
+	Base
+	===
 
-Includes functionality used to manipulate the xui object collection; things like iteration and set operations are included here.
+	Includes functionality used to manipulate the xui object collection; things like iteration and set operations are included here.
 
 */
 var undefined,
@@ -47,38 +47,38 @@ function removex(array, from, to) {
 
 xui.fn = xui.prototype = {
 /**
-extend
----
+	extend
+	---
 
-Allows extension of xui's prototype with the members/methods of the provided object.
+	Allows extension of xui's prototype with the members/methods of the provided object.
 
-__syntax__
+	__syntax__
 
-    xui.extend( object );
+	    xui.extend( object );
 
-Call extend on the xui object to extend all xui instances with functionality and/or members of the passed-in object.
+	Call extend on the xui object to extend all xui instances with functionality and/or members of the passed-in object.
 
-__arguments__
+	__arguments__
 
- - object:object a JavaScript object whose members will be incorporated into xui's prototype
+	 - object:object a JavaScript object whose members will be incorporated into xui's prototype
  
-__example__
+	__example__
 
-Given:
+	Given:
 
-    var thing = {
-        first : function() { return this[ 0 ]; },
-        last : function() { return this[ this.length - 1 ]; }
-    }
+	    var thing = {
+	        first : function() { return this[ 0 ]; },
+	        last : function() { return this[ this.length - 1 ]; }
+	    }
 
-We can extend xui's prototype with these methods by using `extend`:
+	We can extend xui's prototype with these methods by using `extend`:
 
-    xui.extend( thing );
+	    xui.extend( thing );
 
-Now we can use `first` and `last` in all instances of xui:
+	Now we can use `first` and `last` in all instances of xui:
 
-    var f = x$( '.someClass' ).first();
-    var l = x$( '.differentClass' ).last();
+	    var f = x$( '.someClass' ).first();
+	    var l = x$( '.differentClass' ).last();
 
 */
     extend: function(o) {
@@ -87,37 +87,37 @@ Now we can use `first` and `last` in all instances of xui:
         }
     },
 /**
-find
----
+	find
+	---
 
-Finds matching elements based on a query string. The global xui entry `x$` function is a reference to the `find` function.
+	Finds matching elements based on a query string. The global xui entry `x$` function is a reference to the `find` function.
 
-__syntax__
+	__syntax__
 
-    x$(window).find( selector [, context] );
+	    x$(window).find( selector [, context] );
 
-__arguments__
+	__arguments__
 
- - selector:string a CSS selector string to match elements to.
- - context:HTMLElement an html element to use as the "root" element to search from.
+	 - selector:string a CSS selector string to match elements to.
+	 - context:HTMLElement an html element to use as the "root" element to search from.
  
-__example__
+	__example__
 
-Given the following markup:
+	Given the following markup:
 
-    <ul id="first">
-        <li id="one">1</li>
-        <li id="two">2</li>
-    </ul>
-    <ul id="second">
-        <li id="three">3</li>
-        <li id="four">4</li>
-    </ul>
+	    <ul id="first">
+	        <li id="one">1</li>
+	        <li id="two">2</li>
+	    </ul>
+	    <ul id="second">
+	        <li id="three">3</li>
+	        <li id="four">4</li>
+	    </ul>
 
-We can select only specific list items by using `find`, as opposed to selecting off the document root:
+	We can select only specific list items by using `find`, as opposed to selecting off the document root:
 
-    x$('li'); // returns all four list item elements.
-    x$('#second').find('li'); // returns list items "three" and "four"
+	    x$('li'); // returns all four list item elements.
+	    x$('#second').find('li'); // returns list items "three" and "four"
 
 */
     find: function(q, context) {
@@ -171,14 +171,14 @@ We can select only specific list items by using `find`, as opposed to selecting 
     },
 
 /**
-set
----
+	set
+	---
 
-Sets the objects in the xui collection.
+	Sets the objects in the xui collection.
 
-__syntax__
+	__syntax__
 
-    x$(window).set( array );
+	    x$(window).set( array );
 
 */
     set: function(elements) {
@@ -189,25 +189,21 @@ __syntax__
         return ret;
     },
 /**
-reduce
----
+	reduce
+	---
 
-Reduces the set of elements in the xui object to a unique set.
+	Reduces the set of elements in the xui object to a unique set.
 
-__syntax__
+	__syntax__
 
-    x$(someSelector).reduce( [ elements [, toIndex ]] );
+	    x$(someSelector).reduce( [ elements [, toIndex ]] );
 
-The elements
+	The elements parameter is optional - if not specified, will reduce the elements in the current xui object.
 
-__arguments__
+	__arguments__
 
- - elements:Array an array of elements to reduce (optional)
- - 
-
-__example__
-
-
+	 - elements:Array an array of elements to reduce (optional)
+	 - toIndex:Number last index of elements to include in the reducing operation.
 
 */
     reduce: function(elements, b) {
@@ -221,10 +217,37 @@ __example__
 
         return a;
     },
+/**
+	has
+	---
 
-    /**
-     * Has modifies the elements array and reurns all the elements that match (has) a CSS Query
-     */
+	Has modifies the elements array and returns all the elements that match (has) a CSS selector.
+
+	__syntax__
+
+	    x$(someSelector).has( query );
+    
+	Behind the scenes, actually calls the filter method.
+
+	__arguments__
+
+	 - query:string a CSS selector that will match all children of originally-selected xui collection
+ 
+	__example__
+
+	Given
+	
+	    <div>
+	        <div class="gotit">these ones</div>
+	        <div class="gotit">have an extra class</div>
+	    </div>
+	
+	We can use xui like so
+	
+	    var divs = x$('div'); // we've got all four divs from above.
+	    var someDivs = divs.has('.gotit'); // we've now got only the two divs with the class
+
+*/
      has: function(q) {
          var list = xui(q);
          return this.filter(function () {
@@ -236,20 +259,67 @@ __example__
              return found;
          });
      },
-
-    /**
-     * Both an internal utility function, but also allows developers to extend xui using custom filters
-     */
+/**
+	filter
+	---
+	
+	Both an internal utility function, but also allows developers to extend xui using custom filters
+	
+	__syntax__
+	
+	    x$(someSelector).filter( functionHandle );
+	    
+	The `functionHandle` function will get invoked with `this` being the element being iterated on,
+	and the index passed in as a parameter.
+	    
+	__arguments__
+	
+	 - functionHandle:Function a function reference that evaluates to true/false, determining which elements get included in the xui collection.
+	 
+	__example__
+	
+	Perhaps we'd want to filter input elements that are disabled:
+	
+	    x$('input').filter(function(i) {
+	        return this.checked;
+	    });
+	
+*/
     filter: function(fn) {
         var elements = [];
         return this.each(function(el, i) {
             if (fn.call(el, i)) elements.push(el);
         }).set(elements);
     },
-
-    /**
-     * Not modifies the elements array and reurns all the elements that DO NOT match a CSS Query
-     */
+/**
+	not
+	---
+	
+	Not modifies the elements array and returns all the elements that DO NOT match a CSS Query - the opposite of has
+	
+	__syntax__
+	
+	    x$(someSelector).not( someOtherSelector );
+	
+	__arguments__
+	
+	 - someOtherSelector:string a CSS selector that elements should NOT match to.
+	
+	__example__
+	
+	Given
+	
+	    <div>
+	        <div class="gotit">these ones</div>
+	        <div class="gotit">have an extra class</div>
+	    </div>
+	
+	We can use xui like so
+	
+	    var divs = x$('div'); // we've got all four divs from above.
+	    var someDivs = divs.not('.gotit'); // we've now got only the two divs _without_ the class "gotit"
+	
+*/
     not: function(q) {
         var list = slice(this);
         return this.filter(function(i) {
@@ -260,13 +330,27 @@ __example__
             return found;
         });
     },
-
-
-    /**
-     * Element iterator.
-     * 
-     * @return {XUI} Returns the XUI object. 
-     */
+/**
+	each
+	---
+	
+	Element iterator (over the xui collection).
+	
+	__syntax__
+	
+	    x$(window).each( functionHandle )
+	
+	__arguments__
+	
+	 - functionHandle:Function callback function that will execute with each element being passed in as the `this` object and first parameter to callback
+	
+	__example__
+	
+	    x$(someSelector).each(function(element, index, xui) {
+	        alert("Here's the " + index + " element: "+ element);
+	    });
+	
+*/
     each: function(fn) {
         // we could compress this by using [].forEach.call - but we wouldn't be able to support
         // fn return false breaking the loop, a feature I quite like.
