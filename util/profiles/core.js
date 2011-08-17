82658149161be9d5c27e228704b0e0dc02afad7a
Ramin B <i8ramin@gmail.com>
2011-08-17 10:39:55 -0400

    Revert "remove emile and fx b/c we don't use em"
    This reverts commit 70835653673a69ea1e11260de7088214ae19c2b8.

--- util/profiles/core.js
+++ util/profiles/core.js
@@ -5,8 +5,10 @@
         "src/base",
         "src/js/dom",
         "src/js/event",
+        "src/js/fx",
         "src/js/style",
         "src/js/xhr",
+        "packages/emile/emile",
         "packages/domready/ready",
         "src/footer"
     ]
