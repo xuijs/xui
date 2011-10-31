ab0f52b1e13cbc87403c30b880a74b4cc44d92db
Fil Maj <filip.maj@nitobi.com>
2011-10-31 10:42:59 -0700

    IE had outdated dom code

--- src/ie/dom.js
+++ src/ie/dom.js
@@ -1,65 +1,96 @@
 /**
- *
- * @namespace {Dom}
- * @example
- *
- * Dom
- * ---
- *	
- * Manipulating the Document Object Model aka the DOM.
- * 
- */
-xui.extend({
+	DOM
+	===
 
-    /**
-	 * For manipulating HTML markup in the DOM.
-	 *	
-	 * syntax:
-	 *
-	 * 		x$(window).html( location, html );
-	 *
-	 * or this method will accept just an html fragment with a default behavior of inner..
-	 *
-	 * 		x$(window).html( htmlFragment );
-	 * 
-	 * arguments:
-	 * 
-	 * - location:string can be one of inner, outer, top, bottom
-	 * - html:string any string of html markup or HTMLElement
-	 *
-	 * example:
-	 *
-	 *  	x$('#foo').html( 'inner',  '<strong>rock and roll</strong>' );
-	 *  	x$('#foo').html( 'outer',  '<p>lock and load</p>' );
-	 * 		x$('#foo').html( 'top',    '<div>bangers and mash</div>');
-	 *  	x$('#foo').html( 'bottom', '<em>mean and clean</em>');
-	 *  	x$('#foo').html( 'remove');	
-	 *  	x$('#foo').html( 'before', '<p>some warmup html</p>');
-	 *  	x$('#foo').html( 'after', '<p>more html!</p>');
-	 * 
-	 * or
-	 * 
-	 * 		x$('#foo').html('<p>sweet as honey</p>');
-	 * 
-	 */
+	Set of methods for manipulating the Document Object Model (DOM).
+
+*/
+xui.extend({
+/**
+	html
+	----
+
+	Manipulates HTML in the DOM. Also just returns the inner HTML of elements in the collection if called with no arguments.
+
+	### syntax ###
+
+		x$( window ).html( location, html );
+
+	or this method will accept just a HTML fragment with a default behavior of inner:
+
+		x$( window ).html( html );
+
+	or you can use shorthand syntax by using the location name argument as the function name:
+
+		x$( window ).outer( html );
+		x$( window ).before( html );
+	
+	or you can just retrieve the inner HTML of elements in the collection with:
+	
+	    x$( document.body ).html();
+
+	### arguments ###
+
+	- location `String` can be one of: _inner_, _outer_, _top_, _bottom_, _remove_, _before_ or _after_.
+	- html `String` is a string of HTML markup or a `HTMLElement`.
+
+	### example ###
+
+		x$('#foo').html('inner', '<strong>rock and roll</strong>');
+		x$('#foo').html('outer', '<p>lock and load</p>');
+		x$('#foo').html('top',   '<div>bangers and mash</div>');
+		x$('#foo').html('bottom','<em>mean and clean</em>');
+		x$('#foo').html('remove');
+		x$('#foo').html('before', '<p>some warmup html</p>');
+		x$('#foo').html('after',  '<p>more html!</p>');
+
+	or
+
+		x$('#foo').html('<p>sweet as honey</p>');
+		x$('#foo').outer('<p>free as a bird</p>');
+		x$('#foo').top('<b>top of the pops</b>');
+		x$('#foo').bottom('<span>bottom of the barrel</span>');
+		x$('#foo').before('<pre>first in line</pre>');
+		x$('#foo').after('<marquee>better late than never</marquee>');
+*/
     html: function(location, html) {
         clean(this);
 
         if (arguments.length == 0) {
-            return this[0].innerHTML;
+            var i = [];
+            this.each(function(el) {
+                i.push(el.innerHTML);
+            });
+            return i;
         }
         if (arguments.length == 1 && arguments[0] != 'remove') {
             html = location;
             location = 'inner';
         }
-
+        if (location != 'remove' && html && html.each !== undefined) {
+            if (location == 'inner') {
+                var d = document.createElement('p');
+                html.each(function(el) {
+                    d.appendChild(el);
+                });
+                this.each(function(el) {
+                    el.innerHTML = d.innerHTML;
+                });
+            } else {
+                var that = this;
+                html.each(function(el){
+                    that.html(location, el);
+                });
+            }
+            return this;
+        }
         return this.each(function(el) {
             var parent, 
                 list, 
                 len, 
                 i = 0;
-            if (location == "inner") {
-                if (typeof html == string) {
+            if (location == "inner") { // .html
+                if (typeof html == string || typeof html == "number") {
                     el.innerHTML = html;
                     list = el.getElementsByTagName('SCRIPT');
                     len = list.length;
@@ -70,109 +101,103 @@ xui.extend({
                     el.innerHTML = '';
                     el.appendChild(html);
                 }
-            } else if (location == "outer") {
-                el.parentNode.replaceChild(wrapHelper(html, el), el);
-            } else if (location == "top") {
-                el.insertBefore(wrapHelper(html, el), el.firstChild);
-            } else if (location == "bottom") {
-                el.insertBefore(wrapHelper(html, el), null);
-            } else if (location == "remove") {
+            } else {
+              if (location == 'remove') {
                 el.parentNode.removeChild(el);
-            } else if (location == "before") {
-                el.parentNode.insertBefore(wrapHelper(html, el.parentNode), el);
-            } else if (location == "after") {
-                el.parentNode.insertBefore(wrapHelper(html, el.parentNode), el.nextSibling);
+              } else {
+                var elArray = ['outer', 'top', 'bottom'],
+                    wrappedE = wrapHelper(html, (elArray.indexOf(location) > -1 ? el : el.parentNode )),
+                    children = wrappedE.childNodes;
+                if (location == "outer") { // .replaceWith
+                  el.parentNode.replaceChild(wrappedE, el);
+                } else if (location == "top") { // .prependTo
+                    el.insertBefore(wrappedE, el.firstChild);
+                } else if (location == "bottom") { // .appendTo
+                    el.insertBefore(wrappedE, null);
+                } else if (location == "before") { // .insertBefore
+                    el.parentNode.insertBefore(wrappedE, el);
+                } else if (location == "after") { // .insertAfter
+                    el.parentNode.insertBefore(wrappedE, el.nextSibling);
+                }
+                var parent = wrappedE.parentNode;
+                while(children.length) {
+                  parent.insertBefore(children[0], wrappedE);
+                }
+                parent.removeChild(wrappedE);
+              }
             }
         });
     },
-    
-    append: function (html) {
-        return this.html(html, 'bottom');
-    },
-    
-    prepend: function (html) {
-      return this.html(html, 'top');
-    },
 
-    /**
-	 * Attribute getter/setter
-	 *
-	 */
+/**
+	attr
+	----
+
+	Gets or sets attributes on elements. If getting, returns an array of attributes matching the xui element collection's indices.
+
+	### syntax ###
+
+		x$( window ).attr( attribute, value );
+
+	### arguments ###
+
+	- attribute `String` is the name of HTML attribute to get or set.
+	- value `Varies` is the value to set the attribute to. Do not use to get the value of attribute _(optional)_.
+
+	### example ###
+
+	To get an attribute value, simply don't provide the optional second parameter:
+
+		x$('.someClass').attr('class');
+
+	To set an attribute, use both parameters:
+
+		x$('.someClass').attr('disabled', 'disabled');
+*/
     attr: function(attribute, val) {
         if (arguments.length == 2) {
             return this.each(function(el) {
-                el.setAttribute(attribute, val);
+                if (el.tagName && el.tagName.toLowerCase() == 'input' && attribute == 'value') el.value = val;
+                else if (el.setAttribute) {
+                  if (attribute == 'checked' && (val == '' || val == false || typeof val == "undefined")) el.removeAttribute(attribute);
+                  else el.setAttribute(attribute, val);
+                }
             });
         } else {
             var attrs = [];
             this.each(function(el) {
-                var val = el.getAttribute(attribute);
-                if (val != null)
-                attrs.push(val);
+                if (el.tagName && el.tagName.toLowerCase() == 'input' && attribute == 'value') attrs.push(el.value);
+                else if (el.getAttribute && el.getAttribute(attribute)) {
+                    attrs.push(el.getAttribute(attribute));
+                }
             });
             return attrs;
         }
     }
-// --
 });
-
+"inner outer top bottom remove before after".split(' ').forEach(function (method) {
+  xui.fn[method] = function(where) { return function (html) { return this.html(where, html); }; }(method);
+});
 // private method for finding a dom element
 function getTag(el) {
     return (el.firstChild === null) ? {'UL':'LI','DL':'DT','TR':'TD'}[el.tagName] || el.tagName : el.firstChild.tagName;
 }
 
 function wrapHelper(html, el) {
-  return (typeof html == string) ? wrap(html, getTag(el)) : html;
+  if (typeof html == string) return wrap(html, getTag(el));
+  else { var e = document.createElement('div'); e.appendChild(html); return e; }
 }
 
 // private method
 // Wraps the HTML in a TAG, Tag is optional
 // If the html starts with a Tag, it will wrap the context in that tag.
 function wrap(xhtml, tag) {
-
-    var attributes = {},
-        re = /^<([A-Z][A-Z0-9]*)([^>]*)>(.*)<\/\1>/i,
-        element,
-        x,
-        a,
-        i = 0,
-        attr,
-        node,
-        attrList;
-        
-    if (re.test(xhtml)) {
-        result = re.exec(xhtml);
-        tag = result[1];
-
-        // if the node has any attributes, convert to object
-        if (result[2] !== "") {
-            attrList = result[2].split(/([A-Z]*\s*=\s*['|"][A-Z0-9:;#\s]*['|"])/i);
-
-            for (; i < attrList.length; i++) {
-                attr = attrList[i].replace(/^\s*|\s*$/g, "");
-                if (attr !== "" && attr !== " ") {
-                    node = attr.split('=');
-                    attributes[node[0]] = node[1].replace(/(["']?)/g, '');
-                }
-            }
-        }
-        xhtml = result[3];
-    }
-
-    element = document.createElement(tag);
-
-    for (x in attributes) {
-        a = document.createAttribute(x);
-        a.nodeValue = attributes[x];
-        element.setAttributeNode(a);
-    }
-
-    element.innerHTML = xhtml;
-    return element;
+  var e = document.createElement('div');
+  e.innerHTML = xhtml;
+  return e;
 }
 
-
-/**
+/*
 * Removes all erronious nodes from the DOM.
 * 
 */
@@ -188,11 +213,7 @@ function clean(collection) {
             if (n.nodeType == 3 && !ns.test(n.nodeValue)) {
                 d.removeChild(n);
             } else {
-				if(nx!=null && nx.data!=null)
-					n.nodeIndex = ++ni; // FIXME not sure what this is for, and causes IE to bomb (the setter) - @rem
-										// cluster : the 'if' condition I added, allows to solve the problem with IE 
-										// 			 but I don't know how useful this n.nodeIndex is...
-										//			 removing it doesn't seem to bring any problem
+                n.nodeIndex = ++ni; // FIXME not sure what this is for, and causes IE to bomb (the setter) - @rem
             }
             n = nx;
         }
