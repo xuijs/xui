bc782409d65973a2387c3810e64ec6bddb0a9fbd
Alex Black <alex@alexblack.ca>
2012-03-13 15:58:29 -0400

    Patch for Array.indexOf to support IE6/7/8

--- src/base.js
+++ src/base.js
@@ -45,6 +45,19 @@ if (! [].forEach) {
         }
     };
 }
+/* 
+ * Patch indexOf for internet explorer: http://soledadpenades.com/2007/05/17/arrayindexof-in-internet-explorer/ */
+ */
+if(!Array.indexOf){
+  Array.prototype.indexOf = function(obj) {
+    for(var i = 0; i < this.length; i++) {
+      if(this[i] == obj){
+          return i;
+      }
+    }
+    return -1;
+  }
+}
 /*
  * Array Remove - By John Resig (MIT Licensed) 
  */
